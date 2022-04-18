2cf236f27f2a24625211fc1b84e758b6b22906fb
Ajaaypranav Ramaiya Krisshnan <ajaay@zetran.com>
2022-04-18 16:08:10 +0530

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Ajaaypranav R K on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
